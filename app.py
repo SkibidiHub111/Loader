b6dad0b7e87995bc45c238d115531831c50148ea
SkibidiHub111 <pbao010101@gmail.com>
2025-10-18 14:09:53 +0700

    Update Lua script URL in app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def lua_script():
     lua_code = '''
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().Config = getgenv().Config or {}
-local v_1 = loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/.../code_chinh.lua"))()
+local v_1 = loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1428012794678087950/1428998137539854336/6652138009334659.lua?ex=68f489b9&is=68f33839&hm=196d2b650cd4fdbb2f19a895abf0fee4e025f098d8ddde767908677a3284eec2&"))()
 v_1()
 '''
     return Response(lua_code, mimetype='text/plain')
