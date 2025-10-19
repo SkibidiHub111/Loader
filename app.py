c5ffd70dcff80874262add31f5b7529d4046605d
SkibidiHub111 <pbao010101@gmail.com>
2025-10-19 07:40:39 +0700

    Update Lua script URL in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def lua_script():
-    url = "https://cdn.discordapp.com/attachments/1428012794678087950/1428998137539854336/6652138009334659.lua?ex=68f53279&is=68f3e0f9&hm=2dfdfbf80d8a3adf67bc303aadc6ff30ba53ef7880851d2f226d84f774e122e8&"
+    url = "https://raw.githubusercontent.com/SkibidiHub111/Url/refs/heads/main/Cyborg"
     response = requests.get(url)
     if response.status_code == 200:
         lua_code = response.text
